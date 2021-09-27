8b4aab58b2eaa1a87ccc4ae894a369d06593d6ab
Murillo Mendes <murillomendes95@hotmail.com>
2021-09-26 23:27:54 -0300

    Minor changes

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
-
-
 const game = (function () {
+    
+    const allPlayUnits = document.querySelectorAll('.play-unit')
+    
+    let roundNumber = 1
 
     const gameBoard = (function() {
 
-        // const boardContainer = document.querySelector('.gameboard')
-        const allPlayUnits = document.querySelectorAll('.play-unit')
-    
+        function updateBoardOnPlay() {
+            allPlayUnits.forEach(div => div.addEventListener('DOMCharacterDataModified', () => console.log('test')))
+        }
+
         function clearBoard() {
             allPlayUnits.forEach(div => div.textContent = '')
+        }    
+
+        function nextRound() {
+            roundNumber++
+        }
+
+        function playRound(e) {
+            (roundNumber % 2 == 0) ? player1.play.call(e.currentTarget) : player2.play.call(e.currentTarget)
+            checkWin()
+        }
+
+        function checkWin() {
+            if (roundNumber == 10) {
+                console.log('game over');
+                player1.win()
+                console.log(player1.isWinner());
+            }
         }
     
-    
         return {
-            clearBoard
+            clearBoard,
+            nextRound,
+            playRound,
+            updateBoardOnPlay
         }
     })();
 
@@ -29,23 +51,38 @@ const game = (function () {
         
         function play() {
             this.textContent = symbol
+            gameBoard.nextRound()
+            console.log(this);
         }
         
         function win() {
             console.log(`Congratulations, ${name}! You have won!`);
+            winCounter++;
+            roundNumber = 1
         }
 
-        return { name, symbol}
+        function isWinner() {
+            return (winCounter == 3)
+        }
+
+        return { name, symbol, play, win, isWinner, winCounter}
     }
 
-    startGame()
     
     function startGame() {
         gameBoard.clearBoard()
     }
     
-    
-    
+    startGame()
+    gameBoard.updateBoardOnPlay()
+    // const player1 = playerFactory(prompt('Player 1 name'), 'O')
+    // const player2 = playerFactory(prompt('Player 2 name'), 'O')
+    const player1 = playerFactory('Murillo', 'O')
+    const player2 = playerFactory('Érica', 'X')
+
+
+    allPlayUnits.forEach(div => div.addEventListener('click', gameBoard.playRound))
+
     return {
 
     }
